4848d975ebf73142eef3ed57a8cefec1ab3e89a0
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-06-27 17:46:50 +0200

    pull artifacts from gcs

---
+++ .buildkite/scripts/steps/scalability/benchmarking.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source .buildkite/scripts/common/util.sh
+
+.buildkite/scripts/bootstrap.sh
+
+# These tests are running on static workers so we have to make sure we delete previous build of Kibana
+rm -rf "$KIBANA_BUILD_LOCATION"
+
+GCS_BUCKET="gs://kibana-performance/scalability-tests"
+gsutil cp "$GCS_BUCKET/LATEST" .
+HASH=`cat scalability-tests_LATEST`
+gsutil cp "$GCS_BUCKET/$HASH" .
+
+ls -la $HASH
+
+# unset env vars defined in other parts of CI for automatic APM collection of
+# Kibana. We manage APM config in our FTR config and performance service, and
+# APM treats config in the ENV with a very high precedence.
+unset ELASTIC_APM_ENVIRONMENT
+unset ELASTIC_APM_TRANSACTION_SAMPLE_RATE
+unset ELASTIC_APM_SERVER_URL
+unset ELASTIC_APM_SECRET_TOKEN
+unset ELASTIC_APM_ACTIVE
+unset ELASTIC_APM_CONTEXT_PROPAGATION_ONLY
+unset ELASTIC_APM_ACTIVE
+unset ELASTIC_APM_SERVER_URL
+unset ELASTIC_APM_SECRET_TOKEN
+unset ELASTIC_APM_GLOBAL_LABELS
